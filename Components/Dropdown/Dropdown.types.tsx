fa24079330152d886c73943cac0afa7f0b0df2a8
Glen <glenrcoffey@gmail.com>
2020-11-12 21:18:03 +1100

    cleaning up typing in the downshift component

--- Components/Dropdown/Dropdown.types.tsx
+++ Components/Dropdown/Dropdown.types.tsx
@@ -1,8 +1,9 @@
 import { SetStateAction, SyntheticEvent } from "react";
 import { CubicWeightResult } from "pages";
+import { Callback } from "downshift";
 
 export interface DropdownProps {
   options: string[];
   onChange: (selectedItem: string) => void;
-  clear: (event: SyntheticEvent<any, Event>) => SetStateAction<CubicWeightResult>;
+  clear: (event: SyntheticEvent<any, Event>) => Callback;
 }
