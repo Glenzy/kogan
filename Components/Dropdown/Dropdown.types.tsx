5c85c63a2247f5523d78120724920182e75fa801
Glen <glenrcoffey@gmail.com>
2020-11-12 21:09:33 +1100

    fixing typing

--- Components/Dropdown/Dropdown.types.tsx
+++ Components/Dropdown/Dropdown.types.tsx
@@ -1,8 +1,8 @@
-import { SetStateAction } from "react";
-import { CubicWeightResult } from "./pages";
+import { SetStateAction, SyntheticEvent } from "react";
+import { CubicWeightResult } from "pages";
 
 export interface DropdownProps {
   options: string[];
   onChange: (selectedItem: string) => void;
-  clear: () => SetStateAction<CubicWeightResult>;
+  clear: (event: SyntheticEvent<any, Event>) => SetStateAction<CubicWeightResult>;
 }
